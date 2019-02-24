444db8d13333813f4b1a05408a0eaa0e02d6afe5
miiamah <miia.mahonen@metropolia.fi>
2019-02-24 21:53:26 +0200

    Update app2.js

--- js/app2.js
+++ js/app2.js
@@ -1,12 +1,7 @@
 // Initialize Firebase
 (function () {
     var config = {
-        apiKey: "AIzaSyAFD3lOAkpVNiXw0Lj3LOWT7aUBitCYp_c",
-        authDomain: "team-passat.firebaseapp.com",
-        databaseURL: "https://team-passat.firebaseio.com",
-        projectId: "team-passat",
-        storageBucket: "team-passat.appspot.com",
-        messagingSenderId: "825934187463"
+        ...
     };
     firebase.initializeApp(config);
 
